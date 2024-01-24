c003175eab406ed6a3854654ba6ca7d3fd7d44d1
pabloTokuo <pbo_1996@hotmail.com>
2024-01-24 20:53:23 -0300

    exercicios e metade todolist

---
+++ Modulo9/Aula1/exercicios/app.js
@@ -0,0 +1,124 @@
+/*
+  01
+
+  - Exiba no console apenas as letras que a "myString" contém;
+  - Não modifique a string manualmente.
+*/
+
+const myString = "    JS      ";
+
+console.log(myString.trim());
+
+/*
+  02
+
+  - Baseado no score dos objetos, ordene o array "people" de forma crescente 
+    (menores no topo, maiores em baixo);
+  - Não modifique o array original;
+  - Exiba o array ordenado no console.
+*/
+
+const people = [
+  { firstName: "Estevão", lastName: "Rodriguez", score: 90 },
+  { firstName: "José", lastName: "Antônio", score: 100 },
+  { firstName: "Felipe", lastName: "Tavares", score: 71 },
+  { firstName: "Eric", lastName: "Silva", score: 82 },
+];
+
+const orderPeopleByScore = people
+  .map((people) => people)
+  .sort((item1, item2) => item1.score - item2.score);
+
+console.log(orderPeopleByScore);
+
+/*
+  03
+
+  - Gere um novo array com apenas os animais que contém 3 letras no nome;
+
+  Ps: Neste e nos demais exercícios, utilize o debugger para visualizar o valor 
+      final que os exercícios pedem. Após resolver um exercício, remova o 
+      debugger antes de partir para o próximo.
+*/
+
+const animals = ["cão", "gato", "boi", "leão", "gnu", "alce", "ema"];
+
+const animalsWithThreeLetters = animals.filter((animal) => animal.length === 3);
+
+/*
+  04
+
+  - Baseado no array "animals", gere um novo array com a quantidade de letras do 
+    nome de cada animal. Ex.: [6, 8, 2].
+*/
+
+const animalsQuantityLettersName = animals.map((animal) => animal.length);
+
+/*
+  05
+
+  - Através do encadeamento de 2 métodos, gere um novo array com apenas os nomes  
+    dos amigos que moram perto (que contém a propriedade "nearMe" armazenando 
+    true).
+*/
+
+const friends = [
+  { id: 1, name: "João", nearMe: true },
+  { id: 2, name: "Matheus", nearMe: true },
+  { id: 3, name: "Luana", nearMe: false },
+  { id: 4, name: "Nilson", nearMe: true },
+  { id: 5, name: "Solange", nearMe: false },
+];
+
+const friendsNearMe = friends
+  .filter((friend) => friend.nearMe)
+  .map((friend) => friend.name);
+
+/*
+  06
+
+  - Através do encadeamento de 2 métodos, obtenha a soma só dos números ímpares 
+    do array abaixo.
+*/
+
+const numbers = [46, 86, 212, 29, 51, 9, 25, 42, 81];
+
+const oddNumbers = numbers
+  .filter((oddNumber) => oddNumber % 2 === 1)
+  .reduce((acc, number) => {
+    return acc + number;
+  }, 0);
+
+/*
+  07
+
+  - Através do encadeamento de 2 métodos, obtenha a soma da população de todos  
+    países, exceto a China.
+*/
+
+const data = [
+  {
+    country: "China",
+    population: 1409517397,
+  },
+  {
+    country: "India",
+    population: 1339180127,
+  },
+  {
+    country: "USA",
+    population: 324459463,
+  },
+  {
+    country: "Indonesia",
+    population: 263991379,
+  },
+];
+
+const newDataWithoutChina = data
+  .filter((data) => data.country !== "China")
+  .reduce((acc, data) => {
+    return acc + data.population;
+  }, 0);
+
+debugger;
