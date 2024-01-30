fb61f2ab6e946807b798451818be87bf7bf90498
pabloTokuo <pbo_1996@hotmail.com>
2024-01-30 18:55:02 -0300

    modulo 9 finalizado

--- Modulo9/Aula1/app.js
+++ Modulo9/Aula1/app.js
@@ -2,45 +2,64 @@ const formAddTodo = document.querySelector(".form-add-todo");
 const inputSearch = document.querySelector(".form-search input");
 const todosContainer = document.querySelector(".todos-container");
 
+const addTodo = (inputValue) => {
+  if (inputValue.length) {
+    todosContainer.innerHTML += `
+    <li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${inputValue}">
+      <span>${inputValue}</span>
+      <i class="far fa-trash-alt" data-trash="${inputValue}"></i>
+    </li>
+    `;
+  }
+}
+
 formAddTodo.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const inputValue = event.target.add.value.trim();
+  addTodo(inputValue);
 
-  if (inputValue.length) {
-    todosContainer.innerHTML += `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-      <span>${inputValue}</span>
-      <i class="far fa-trash-alt delete"></i>
-    </li>
-    `;
-
-    event.target.reset();
-  }
+  event.target.reset();
 });
+
+const removeTodo = (clickedElement) => {
+  const trashDataValue = clickedElement.dataset.trash;
+  const todo = document.querySelector(`[data-todo="${trashDataValue}"]`);
+
+  if(trashDataValue) {
+    todo.remove();
+  }
+}
 
 todosContainer.addEventListener("click", (event) => {
   const clickedElement = event.target;
-
-  if(clickedElement.classList.contains("delete")) {
-    clickedElement.parentNode.remove()
-  }
+  removeTodo(clickedElement);
 });
 
-inputSearch.addEventListener("input", (event) => {
-  const inputValue = event.target.value.trim().toLowerCase();
-  Array.from(todosContainer.children)
+const hideTodos = (todos, inputValue) => {
+  todos
   .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
   .forEach(todo => {
     todo.classList.remove("d-flex");
     todo.classList.add("hidden");
   });
+};
 
-  Array.from(todosContainer.children)
+const showTodos = (todos, inputValue) => {
+  todos
   .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
   .forEach(todo => {
     todo.classList.add("d-flex");
     todo.classList.remove("hidden");
   });
+};
+
+inputSearch.addEventListener("input", (event) => {
+  const inputValue = event.target.value.trim().toLowerCase();
+  const todos = Array.from(todosContainer.children);
+
+  hideTodos(todos, inputValue);
+
+  showTodos(todos, inputValue);
 
 })
